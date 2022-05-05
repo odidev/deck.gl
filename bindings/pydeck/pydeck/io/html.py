58158171efc425dec1f4a2d65d9930ae42d58981
odidev <odidev@puresoftware.com>
2022-05-05 08:13:48 +0530

    Update html.py

--- bindings/pydeck/pydeck/io/html.py
+++ bindings/pydeck/pydeck/io/html.py
@@ -45,7 +45,7 @@ def cdn_picker(offline=False):
             "<script type='text/javascript' src='http://localhost:{dev_port}/dist/index.js.map'></script>\n"
         ).format(dev_port=dev_port)
     if offline:
-        RELPATH_TO_BUNDLE = "../nbextension/static/index.js"
+        RELPATH_TO_BUNDLE = "../nbextension/static/extensionRequires.js"
         with open(join(dirname(__file__), RELPATH_TO_BUNDLE), "r", encoding="utf-8") as file:
             js = file.read()
         return "<script type='text/javascript'>{}</script>".format(js)
